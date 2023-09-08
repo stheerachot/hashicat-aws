c05284b6f5af4e0a135b387ee3f333f4ee72b136
stheerachot <50516044+stheerachot@users.noreply.github.com>
2023-09-08 18:00:30 +0700

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,4 @@ module "s3-bucket" {
   source  = "app.terraform.io/WestconTH/s3-bucket/aws"
   version = "2.8.0"
 }
-credentials "app.terraform.io" {
-  # valid user API token
-  token = "obFHZhHMn8t0lg.atlasv1.ALX2HbgPbrBbuxEKqWgmiYH48ZEzG9mTjbDwRzghq4qCmiqSZSn3NN5HutBWqHQJVVU"
-}
+
