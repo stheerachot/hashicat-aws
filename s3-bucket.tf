eb8f66da08a018be0e0fbdb44cddec3d69f61ae2
stheerachot <50516044+stheerachot@users.noreply.github.com>
2023-09-08 18:02:54 +0700

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,3 +1,14 @@
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+}
 module "s3-bucket" {
   source  = "app.terraform.io/WestconTH/s3-bucket/aws"
   version = "2.8.0"
